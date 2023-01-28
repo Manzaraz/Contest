36fb5193a70ff47dd8aa2bb1a1c5b9ed39cd00ec
Christian Manzaraz <magnusmanz@gmail.com>
2023-01-28 18:47:01 -0300

    Create the Animation and Implement the Animation

--- Contest/ViewController.swift
+++ Contest/ViewController.swift
@@ -17,7 +17,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func enterButtonDidTouchUpInside(_ sender: Any) {
+        if emailTextField.text?.isEmpty == true {
+            performLabTextFieldShakeAnimation()
+        } else {
+            performSegue(withIdentifier: "toEnteredView", sender: nil)
+        }
     }
     
+    private func performLabTextFieldShakeAnimation() {
+        UIView.animate(withDuration: 0.2, animations: {
+            let rightTransform = CGAffineTransform(translationX: 20, y: 0)
+            self.emailTextField.transform = rightTransform
+        }) {(_) in
+            UIView.animate(withDuration: 0.2, animations: {
+                self.emailTextField.transform = CGAffineTransform.identity
+            })
+        }
+    }
 }
 
